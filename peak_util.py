5d24d6b6e267a94f62e28d9b51b6f3e36bf269e1
Anwesh Bhattacharya <anwesh0304@gmail.com>
2020-01-14 02:45:29 +0530

    Update peak_util.py

--- peak_util.py
+++ peak_util.py
@@ -94,7 +94,7 @@ def double_snr_peak_mark (Z_flip, rows, px1, py1, snr1, px2, py2, snr2, cutout, 
 	# 	 - thicc marking boolean value
 	#
 	# Output - Returns the flipped image data post peak marking
-	#	   and undergoing SNR checks
+	#	   and undergoing SNR checks.
 	############################################################
 	
 	
@@ -279,6 +279,9 @@ def get_noise_cutout (peak_plot_name, levels, env_level, Z_ctr_uq, Z_ctr) :
 			break
 
 	############################################################
+	# The noise list is randomly sampled 10 times and average noise
+	# value is calculated. Then an ensemble average is returned.
+	############################################################
 
 	avg_noise = 0
 	for i in range(0, 10) :
@@ -292,28 +295,43 @@ def get_noise_cutout (peak_plot_name, levels, env_level, Z_ctr_uq, Z_ctr) :
 
 	return (avg_noise, cutout)
 
+
+
 # Note - The algorithm can be improved by reworking the len(reg1) > len(reg2) condition. It's too strict, but lite?
 def peak_plot (peak_dist , Z_regions , Z_img , Z_ctr, Z_ctr_uq, levels, env_level, peak_plot_name , thicc, done_file) :
 	############################################################
 	# Inputs - peak histogram
-	#		 - list of connected high regions
-	#		 - png data
-	#		 - name of the file to mark peaks
-	#		 - thick marking or thin marking truth value
+	#	 - list of connected high regions
+	#	 - smooth image data
+	#	 - contour image data
+	#	 - contour level pixel values
+	#	 - levels of contour
+	#	 - environment level
+	#	 - name of file that saves that peak plot
+	#	 -  thick marking or thin marking truth value
 
-	# Exports one or two png files in which the peaks are marked
-	# If there's only one peak, it exports a _only.png
-	# If there is more than one peak, it exports an _all.png and
-	# _top_pair.png file
-
+	# Output - Exports one or two png files in which the peaks are marked
+	# 	   If there's only one peak, it exports a file whose name
+	#	   is the objID and is appended by '_only.png'
+	#	   For a double peak, it is '_top_pair.png'
+	#	   For no deteced peak, it is '_none.png'
+	#	 - Returns a string that contains the nature of the peak
 	############################################################
 
 	# Flipping png data read by imread ()
 	Z_flip = np.ndarray (shape = Z_img.shape)
 	rows = Z_img.shape[0]
 	for i in range(0 , rows) :
-		Z_flip[i] = Z_img[rows - i - 1] ;
-
+		Z_flip[i] = Z_img[rows - i - 1]
+		
+	############################################################
+	# The following condition traces back to env_level_contour_search
+	# in the env_level_peak_search.py
+	# In that module, if the list of high contour regions is empty,
+	# then gradient ascent is skipped altogether.
+	#
+	# Thus there is no distinct signal to begin with and no peaks.
+	############################################################
 	if len(Z_regions) == 0 :
 		fig = plt.figure ()
 		ax = fig.add_subplot (111)
@@ -329,14 +347,18 @@ def peak_plot (peak_dist , Z_regions , Z_img , Z_ctr, Z_ctr_uq, levels, env_leve
 
 		return "NoPeak"
 
-	############################################################	
-
+	# Obtain the average noise and cutout
 	avg_noise , cutout = get_noise_cutout (peak_plot_name, levels, env_level, Z_ctr_uq, Z_ctr)
 	no_peak = len(peak_dist)
 
+	# Check the definition of get_noise_cutout for the 'NoNoise' condition.
 	if avg_noise == None :
 		return "NoNoise"
 	
+	############################################################
+	# The large section of code-below is the heart of the decision
+	# making process to determine the number of peaks in the object
+	# image.
 	############################################################
 
 	peak_plot_ret = 'Single'
@@ -348,6 +370,7 @@ def peak_plot (peak_dist , Z_regions , Z_img , Z_ctr, Z_ctr_uq, levels, env_leve
 		py2 = peak_dist[1][1]
 
 		#Be careful about using this. Peak may exist at the boundary
+		# Calculating peak average signal values and SNR.
 		signal1 = get_avg_neighvals (px1,py1,cutout)
 		signal2 = get_avg_neighvals (px2,py2,cutout)
 		snr1 = signal1/avg_noise
@@ -358,7 +381,7 @@ def peak_plot (peak_dist , Z_regions , Z_img , Z_ctr, Z_ctr_uq, levels, env_leve
 		reg1 = None
 		reg2 = None
 
-		# Finding the regions in which the two peaks belong ...
+		# Finding the regions in which the two peaks belong
 		for reg in Z_regions :
 			if (px1 , py1) in reg :
 				reg1 = reg 
@@ -370,62 +393,65 @@ def peak_plot (peak_dist , Z_regions , Z_img , Z_ctr, Z_ctr_uq, levels, env_leve
 			if (not reg1 == None) and (not reg2 == None) and reg1 == reg2 :
 				same_region = True
 				break 
-
-		# If the two peaks are not in the same connected region, then they are less likely
-		# to belong to the same envelope and hence the two peaks are reported as separate objects ...
+		
+		############################################################
+		# If the two peaks are not in the same connected region, then 
+		# they are less likely to belong to the same envelope and only
+		# one of them is the true single peak depending on which one of them
+		# belongs to a larger connected region.
+		############################################################
 		if not same_region :
+			
+			# This is no expected to occur, but it happened once. Should be very rare.
 			if (reg1 == None or reg2 == None) :
-				#print ("Algorithm failed")
 				return "Failed"
 
 			peak_plot_ret = 'Single' 		# Truth value signifying that there is only one peak
 
-			# Selecting region with larger number ofpoints ...
+			# Selecting region with larger number of points
 			if len(reg1) > len(reg2) :
-				if snr1 >= 3 :
+				if snr1 >= 3 : # Mark peak only if SNR is greater than 3.
 					Z_flip = peak_mark (Z_flip, rows, px1, py1, cutout, done_file, thicc)
-				else :
+				else :	# Otherwise there is no peak
 					peak_plot_ret = 'NoPeak' 
-					#return None 
 
-			# Selecting region with larger number of points ...			
+			# Selecting region with larger number of points			
 			elif len(reg2) > len(reg1) :
-				if snr2 >= 3 :
+				if snr2 >= 3 : # Mark peak only if SNR is greater than 3.
 					Z_flip = peak_mark (Z_flip, rows, px2, py2, cutout, done_file, thicc)
-				else :
+				else : # Otherwise there is no peak
 					peak_plot_ret = 'NoPeak'
 					#return None
 
-			# If regions have same length, then there are actually two peaks ...
+			# If regions have same length, then there are actually two peaks.
 			else :
-				Z_flip, peak_plot_ret = double_snr_peak_mark (Z_flip, rows, px1, py1, px2, py2, snr2, cutout, done_file, thicc)
-				#if peak_plot_ret == None :
-					#return None 
+				Z_flip, peak_plot_ret = double_snr_peak_mark (Z_flip, rows, px1, py1, px2, py2, snr2, 
+									      cutout, done_file, thicc)
 		else :
 			# This case is needed in case of crowding of peaks around a boundary
 
-			border_problem_1 = border_problem_decide (px1 , py1 , Z_regions)		# If first peak is at boundary
-			border_problem_2 = border_problem_decide (px2 , py2 , Z_regions)		# If second peak is at boundary
+			border_problem_1 = border_problem_decide (px1 , py1 , Z_regions)		
+			border_problem_2 = border_problem_decide (px2 , py2 , Z_regions)		
 
 			if not border_problem_1 and border_problem_2 :
-				if snr1 >= 3 :
+				if snr1 >= 3 : # Mark peak only if SNR is greater than 3.
 					Z_flip = peak_mark (Z_flip, rows, px1, py1, cutout, done_file, thicc)
 					peak_plot_ret = 'Single' 
-				else :
+				else : # Otherwise there is no peak
 					peak_plot_ret = 'NoPeak' 
 					#return None
 			elif border_problem_1 and not border_problem_2 :
-				if snr2 >= 3 :
+				if snr2 >= 3 : # Mark peak only if SNR is greater than 3.
 					Z_flip = peak_mark (Z_flip, rows, px2, py2, cutout, done_file, thicc)
 					peak_plot_ret = 'Single' 
-				else :
+				else : # Otherwise there is no peak
 					peak_plot_ret = 'NoPeak'
 					#return None
 			else :	#No border problem. This is supposed to be the normal case
-				Z_flip, peak_plot_ret = double_snr_peak_mark (Z_flip, rows, px1, py1, snr1, px2, py2, snr2, cutout, done_file, thicc)
-				#if peak_plot_ret == None :
-					#return None 
+				Z_flip, peak_plot_ret = double_snr_peak_mark (Z_flip, rows, px1, py1, snr1, px2, py2, snr2, 
+									      cutout, done_file, thicc)
 
+		# Creating plots after deciding on nature of peaks
 		fig = plt.figure ()
 		ax = fig.add_subplot (111)
 		ax.axis('off')
@@ -443,16 +469,20 @@ def peak_plot (peak_dist , Z_regions , Z_img , Z_ctr, Z_ctr_uq, levels, env_leve
 
 		plt.close (fig)
 	elif no_peak == 1 :	
+		# Finding peak points
 		px1 = peak_dist[0][0]
 		py1 = peak_dist[0][1]
+		
+		# Finding peak signal value and SNR
 		signal1 = get_avg_neighvals (px1,py1,cutout)
 		snr1 = signal1/avg_noise
 
-		if snr1 >= 3 :
+		if snr1 >= 3 : # Mark peak only if SNR is greater than 3.
 			Z_flip = peak_mark (Z_flip, rows, px1, py1, cutout, done_file, thicc)
-		else :
+		else : #  Otherwise there is no peak
 			peak_plot_ret = 'NoPeak'
 
+		# Creating plots
 		fig = plt.figure ()
 		ax = fig.add_subplot (111)
 		ax.axis('off')
@@ -467,10 +497,12 @@ def peak_plot (peak_dist , Z_regions , Z_img , Z_ctr, Z_ctr_uq, levels, env_leve
 			plt.savefig (peak_plot_name + "_none.png" ,transparent = True, bbox_inches = 'tight', pad_inches = 0)
 		plt.close (fig)
 	else :
+		# There is no peak
 		peak_plot_ret = 'NoPeak'
+		
+		# Creating plots
 		fig = plt.figure ()
 		ax = fig.add_subplot (111)
-
 		ax.axis('off')
 		ax.margins(0,0)
 		ax.xaxis.set_major_locator(plt.NullLocator())

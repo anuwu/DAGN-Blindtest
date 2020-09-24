c04767c68d8474039742c37bad4e3532fc80a416
Anwesh Bhattacharya <anwesh0304@gmail.com>
2020-09-24 21:35:25 +0530

    Added a dummy galaxy class. Working on it

---
+++ galaxy.py
@@ -0,0 +1,6 @@
+class Galaxy () :
+    """Contains all of the galaxies info"""
+    
+    def __init__ (self, objid) :
+        """Simple constructor. Other attributes populated with methods"""
+        self.objid = objid
